7808194c991d91276949f80d8277755a436aa6df
Patryzzjuhh <patryzzjuhh@gmail.com>
2023-10-15 17:01:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'find.the.shrooms',
+  appName: 'find-the-shrooms',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
