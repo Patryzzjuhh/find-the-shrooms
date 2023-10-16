37082865dab98f7f35604e2956d991822f1761c2
Patryzzjuhh <patryzzjuhh@gmail.com>
2023-10-16 12:35:37 +0200

    had to add import of  common module in tab2.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent,CommonModule]
 })
 export class Tab2Page {
 
